d5c4a227032d693d6f1ae2ead5167a468dbcf138
Francesco Sullo <sullof@gmail.com>
2019-01-22 17:25:13 -0800

    More scripts

---
+++ gettxTo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -X POST  http://127.0.0.1:8091/walletextension/gettransactionstothis -d '{"account" : {"address" : "$1"}, "offset": 0, "limit": 10}'
---
+++ gettxid.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl http://127.0.0.1:9091/wallet/gettransactionbyid?value=$1
