3998a5e2c50f752314001f6db34374d4c6770da6
Francesco Sullo <sullof@gmail.com>
2021-07-28 19:21:44 -0700

    Changes to purgeNode_modules.sh

--- purgeNode_modules.sh
+++ purgeNode_modules.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
+if [[ -d "node_modules" ]]; then
+	echo "Purging node_modules"
+	rm -rf node_modules
+fi
+
 for file in */ ; do
-  if [[ -d "$file" && ! -L "$file" ]]; then
-  	echo "Purging "$file"node_modules"
-    rm -rf $file"node_modules"
-  fi
+#	if [[ -d "$file" && ! -L "$file" ]]; then
+  		if [[ -d $file"node_modules" ]]; then
+  			echo "Purging "$file"node_modules"
+    		rm -rf $file"node_modules"
+    	fi
+#  	fi
 done
