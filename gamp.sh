76f5f77be791d9844d8de5ae97427535231bcc84
Francesco Sullo <sullof@gmail.com>
2022-11-11 13:12:43 -0800

    No message

--- gamp.sh
+++ gamp.sh
@@ -6,8 +6,13 @@ if [[ "$2" == "--skip" ]]; then
 	NOVERIFY="--no-verify"
 fi
 
+MESSAGE=$1
+if [[ "$1" == "" ]]; then
+	MESSAGE="No message :-/"
+fi
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 git add -A
-git commit $NOVERIFY -m "$1"
+git commit $NOVERIFY -m "$MESSAGE"
 git push origin $branch
 
