bbf60f00ed1f7e69c4ff0a604a8e0fd4028e9632
Francesco Sullo <sullof@gmail.com>
2023-12-11 16:31:38 -0800

    Fix scripts

--- unzip-from-zip.sh
+++ unzip-from-zip.sh
@@ -7,6 +7,8 @@ SOURCE_DIR="$1"
 # If not provided, files will be unzipped in the current directory
 DESTINATION_DIR="$2"
 
+START_FROM="$3"
+
 # Check if the source directory is specified
 if [ "$SOURCE_DIR" == "" ]; then
     echo "Source directory not specified. Exiting..."
@@ -17,6 +19,11 @@ fi
 if [ ! -d "$SOURCE_DIR" ]; then
     echo "Source directory does not exist. Exiting..."
     exit 1
+fi
+
+STARTED=
+if [ "$START_FROM" == "" ]; then
+	STARTED=1
 fi
 
 # Check if the destination directory is specified, if not, use the current directory
@@ -32,6 +39,15 @@ fi
 # Loop through each .tar.gz file in the source directory and unzip it
 for FILE in "$SOURCE_DIR"/*.tar.gz; do
     if [ -f "$FILE" ]; then
+    	BASE_FILE=$(basename "$FILE")
+		if [ "$STARTED" == "" ]; then
+			if [ "$BASE_FILE" == "$START_FROM" ]; then
+				STARTED=1
+			else
+				continue
+			fi
+		fi
+
         echo "Unzipping $(basename "$FILE") to $DESTINATION_DIR..."
         tar -xzf "$FILE" -C "$DESTINATION_DIR"
     fi
--- zip-and-copy.sh
+++ zip-and-copy.sh
@@ -3,6 +3,8 @@
 # Destination directory for the compressed folders
 DESTINATION_DIR="$1"
 
+START_FROM="$2"
+
 if [ "$DESTINATION_DIR" == "" ]; then
 	echo "Destination directory not specified. Exiting..."
 	exit 1
@@ -12,13 +14,22 @@ if [ ! -d "$DESTINATION_DIR" ]; then
     mkdir -p "$DESTINATION_DIR"
 fi
 
-# Loop through each subfolder in the current directory
+STARTED=
+if [ "$START_FROM" == "" ]; then
+	STARTED=1
+fi
+
 for DIR in */; do
     if [ -d "${DIR}" ]; then
         # Remove trailing slash
         DIR_NAME=$(basename "${DIR}")
-        if [ "$DIR_NAME" == "Temp" ]; then
-			continue
+
+		if [ "$STARTED" == "" ]; then
+			if [ "$DIR_NAME" == "$START_FROM" ]; then
+				STARTED=1
+			else
+				continue
+			fi
 		fi
 		echo "Zipping $DIR_NAME..."
         tar -czf "$DESTINATION_DIR/${DIR_NAME}.tar.gz" "${DIR_NAME}"
